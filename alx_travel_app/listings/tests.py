39c2729d953d245478c3a8a946ff2a7ae4a3aac1
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-07 19:20:18 +0200

    project setup

--- alx_travel_app/listings/tests.py
+++ alx_travel_app/listings/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+import os
+
+file_path = "alx_travel_app/requirements.txt"
+
+if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+    print(f"'{file_path}' exists and is not empty")
+else:
+    print(f"'{file_path}' does not exist or is empty")
