fb66e23a65834185cf849fadca1a4efbcd10aa08
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-07 19:11:18 +0200

    project setup

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def test_api(request):
+    return Response({"message": "ALX Travel App API is working!"})
